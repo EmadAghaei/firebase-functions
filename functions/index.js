6b8270dc109520ecd360a512b187b8fd66aa1285
Emad Aghayi <emad.aghayi@gmail.com>
2018-03-11 00:49:12 -0500

    CRUD
    add fucntions for create, get and update of data base

--- functions/index.js
+++ functions/index.js
@@ -10,23 +10,56 @@ exports.addVar = functions.https.onRequest((req, res) => {
     // Grab the text parameter.
     var varKey = req.query.varKey;
     var varValue = req.query.varValue;
+    var googleId = req.query.googleId;
+    var dataStoreId = req.query.dataStoreId;
 // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    var ref = admin.database().ref('/variables');
-    ref.push({varKey: varKey, varValue: varValue});
+    var ref = admin.database().ref('/variables/' + googleId +'/'+dataStoreId);
+    // ref.push({varKey: varKey, varValue: varValue});
+    ref.push({varKey : varKey, varValue: varValue});
     return res.send('Saved data store');
+});
 
 
-})
-;
+exports.getAlldataOfDataStore = functions.https.onRequest((req, res) => {
+    var googleId = req.query.googleId;
+    var ref = admin.database().ref('/variables/' + googleId +'/'+req.query.dataStoreId.toString());
 
+   ref.on("value", function (snapshot) {
+        console.log(snapshot.val());
+        res.status(200).send(snapshot.val());
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+});
 
-exports.getVariables = functions.https.onRequest((req, res) => {
-    var ref = admin.database().ref('/variables');
+exports.getAlldataOfUser = functions.https.onRequest((req, res) => {
+    var googleId = req.query.googleId;
+    var  ref = admin.database().ref('/variables/' + googleId);
     ref.on("value", function (snapshot) {
         console.log(snapshot.val());
         res.status(200).send(snapshot.val());
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
-})
-;
+});
+
+exports.updateVar = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    var varKey = req.query.varKey;
+    var varValue = req.query.varValue;
+    var googleId = req.query.googleId;
+    var dataStoreId = req.query.dataSoreId;
+// Push the new message into the Realtime Database using the Firebase Admin SDK.
+    var ref = admin.database().ref('/variables/' + googleId+'/'+dataStoreId.toString());
+
+    ref.orderByChild('varKey').equalTo(varKey).on("value", function (snapshot) {
+        console.log(snapshot.val());
+
+        snapshot.forEach(function(child) {
+            child.ref.update({varValue: varValue.toString()});
+        });
+        res.status(200).send(snapshot.val());
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+});
